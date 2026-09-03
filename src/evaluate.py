0f5c5cb3f36a6bd74deb53748101260456f6a78b
Abhishek <apple@Apples-MacBook-Pro.local>
2026-09-03 13:12:45 +0530

    added evaluation.py where model gets evaluated

---
+++ src/evaluate.py
@@ -0,0 +1,12 @@
+
+from sklearn.metrics import accuracy_score, roc_auc_score, classification_report, confusion_matrix
+
+def evaluate(clf, X_test, y_test):
+    y_pred = clf.predict(X_test)
+    y_prob = clf.predict_proba(X_test)[:,1]
+
+    print(classification_report(y_test, y_pred))
+    print("ROC-AUC: ", roc_auc_score(y_test, y_prob))
+    print(confusion_matrix(y_test, y_pred))
+
+    
