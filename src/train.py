0109ff753dfea7a91a442643a4862c074f4c73d8
Abhishek <apple@Apples-MacBook-Pro.local>
2026-09-04 13:06:15 +0530

    added xgboost to the training.py

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
 def training(filepath):
 
@@ -31,4 +32,17 @@ def training_rf(filePath):
     rf.fit(X_train, y_train)
 
     return rf, X_test, y_test
+
+def training_xgboost(filePath):
+
+    X, y, preprocessor = load_and_preprocess(filePath)
+    xg = Pipeline(steps=[
+        ('preprocessor', preprocessor),
+        ('xgboost', XGBClassifier(n_estimators=100, learning_rate=0.1, random_state=42))
+    ])
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+    xg.fit(X_train, y_train)
+
+    return xg, X_test, y_test
 
