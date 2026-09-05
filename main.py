32a83785cc45e32935cbb9e8e7dfd88e1d86ccd0
kulkarni-abhishek196 <kulkarniabhishek351@gmail.com>
2026-09-05 14:48:18 +0530

    Added the trained model to the directory

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from src.train import training, training_rf, training_xgboost
 from src.evaluate import evaluate, evaluate_rf, evaluate_xgboost
 from src.explain import explain_model
+import os
+import joblib
+
 
 clf, X_test, y_test = training_xgboost('data/ecomerce_data_E_Comm.csv')
+
+os.makedirs('models', exist_ok=True)
+joblib.dump(clf, 'models/churn_model.pkl')
+print("Model saved.")
+
 evaluate_xgboost(clf, X_test, y_test)
-explain_model(clf, X_test)
 
